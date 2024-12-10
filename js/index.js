70b9417e9c78f797637690e8cc34688e908fb1f9
morganekoval <koval.morgane@gmail.com>
2024-12-10 13:19:44 +0100

    11 12

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ function getFrenchDateAsString(today=new Date()) {
 function getDateAsFilename(today = new Date()) {
 
 	// DEBUG ONLY
-	// today = new Date(2024, 11, 9);
+	// today = new Date(2024, 11, 12);
 	
 	return `./${today.getMonth()+1}/${today.getDate()}.html`;
 }
